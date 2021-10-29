cb74788de77ef2b76d04ff919c15fee4b7ef170f
Erdem Alptuğ Öksüz <erdemalpoksuz@gmail.com>
2021-10-29 15:12:28 +0300

    Created and modified.

---
+++ main.js
@@ -0,0 +1,54 @@
+class Form {
+    steps = []
+    constructor() {
+        this.swiper = new Swiper(".mySwiper", {
+            direction: "vertical"
+        });
+    }
+    step(step) {
+        this.steps.push(step)
+        return this
+    }
+    input(step) {
+        return `<input 
+                type="date" 
+                ${step.required ? 'required' : ''} 
+                placeholder="${step.placeholder}"
+                />`
+    }
+    generate(step) {
+        if (!step.hasOwnProperty('type')) {
+            step.type = 'input'
+        }
+        if (!step.hasOwnProperty('placeholder')) {
+            step.placeholder = 'Cevabınızı buraya yazınız.'
+        }
+        let field = this[step.type](step)
+        let template = document.getElementById('slide-template').innerHTML
+        console.log(template)
+    }
+    start() {
+        this.generate(this.steps[0])
+    }
+}
+
+const form = new Form()
+form.step({
+    name: 'name',
+    title: 'Köylü adını giriniz.',
+    required: true
+}).step({
+    name: 'surname',
+    title: 'Köylü soyadını giriniz.',
+    required: true
+}).step({
+    name: 'gender',
+    type: 'select',
+    values:{
+        1: 'Kadın',
+        2: 'Erkek'
+    },
+    required: true
+})
+
+form.start()
